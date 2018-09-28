348c6c0864f75e8888fa1f34130bf975c3fae67e
Mosè Raguzzini <mose.raguzzini@gmail.com>
2018-09-28 15:43:12 +0200

    added linter, fixed indent & line breaks

--- js/eddb.js
+++ js/eddb.js
@@ -1,4 +1,5 @@
-﻿/*
+﻿/* eslint-disable */
+/*
 E:D Shipyard was created using assets and imagery from Elite: Dangerous, with the permission of Frontier Developments plc, for non-commercial purposes.
 It is not endorsed by nor reflects the views or opinions of Frontier Developments and no employee of Frontier Developments was involved in the making of it.
 
