797d20d21b2bde98d2be3293054589a4a917fe2c
Mosè Raguzzini <mose.raguzzini@gmail.com>
2018-09-29 19:21:02 +0200

    Added missing Meta Alloy Hull Reinforcement Package fids

--- js/eddb.js
+++ js/eddb.js
@@ -2905,16 +2905,16 @@ var eddb = {
 		 4540 : { mtype:'ihrp', cost:   450000, name:'Hull Reinforcement Package', class:5, rating:'D', mass:16.00, hullrnf:390, kinres:2.5, thmres:2.5, expres:2.5, fdid:128668546, fdname:'Int_HullReinforcement_Size5_Class2' },
 		
 		
-		 9151 : { mtype:'imahrp', cost:  7500, name:'Meta Alloy Hull Reinforcement Package', class:1, rating:'E', mass:  2, hullrnf: 72, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size1_Class1' }, // TODO: fdid
-		 9141 : { mtype:'imahrp', cost: 22500, name:'Meta Alloy Hull Reinforcement Package', class:1, rating:'D', mass:  2, hullrnf: 99, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size1_Class2' }, // TODO: fdid
-		 9251 : { mtype:'imahrp', cost: 18000, name:'Meta Alloy Hull Reinforcement Package', class:2, rating:'E', mass:  4, hullrnf:135, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size2_Class1' }, // TODO: fdid
-		 9241 : { mtype:'imahrp', cost: 54000, name:'Meta Alloy Hull Reinforcement Package', class:2, rating:'D', mass:  2, hullrnf:171, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size2_Class2' }, // TODO: fdid
-		 9351 : { mtype:'imahrp', cost: 42000, name:'Meta Alloy Hull Reinforcement Package', class:3, rating:'E', mass:  8, hullrnf:207, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size3_Class1' }, // TODO: fdid
-		 9341 : { mtype:'imahrp', cost:126000, name:'Meta Alloy Hull Reinforcement Package', class:3, rating:'D', mass:  4, hullrnf:234, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size3_Class2' }, // TODO: fdid
-		 9451 : { mtype:'imahrp', cost: 97500, name:'Meta Alloy Hull Reinforcement Package', class:4, rating:'E', mass: 16, hullrnf:270, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size4_Class1' }, // TODO: fdid
-		 9441 : { mtype:'imahrp', cost:292500, name:'Meta Alloy Hull Reinforcement Package', class:4, rating:'D', mass:  8, hullrnf:297, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size4_Class2' }, // TODO: fdid
-		 9551 : { mtype:'imahrp', cost:225000, name:'Meta Alloy Hull Reinforcement Package', class:5, rating:'E', mass: 32, hullrnf:324, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size5_Class1' }, // TODO: fdid
-		 9541 : { mtype:'imahrp', cost:675000, name:'Meta Alloy Hull Reinforcement Package', class:5, rating:'D', mass: 16, hullrnf:351, caures:3.0, fdid:null, fdname:'Int_MetaAlloyHullReinforcement_Size5_Class2' }, // TODO: fdid
+		 9151 : { mtype:'imahrp', cost:  7500, name:'Meta Alloy Hull Reinforcement Package', class:1, rating:'E', mass:  2, hullrnf: 72, caures:3.0, fdid:128793117, fdname:'Int_MetaAlloyHullReinforcement_Size1_Class1' }, // TODO: fdid
+		 9141 : { mtype:'imahrp', cost: 22500, name:'Meta Alloy Hull Reinforcement Package', class:1, rating:'D', mass:  2, hullrnf: 99, caures:3.0, fdid:128793118, fdname:'Int_MetaAlloyHullReinforcement_Size1_Class2' }, // TODO: fdid
+		 9251 : { mtype:'imahrp', cost: 18000, name:'Meta Alloy Hull Reinforcement Package', class:2, rating:'E', mass:  4, hullrnf:135, caures:3.0, fdid:128793119, fdname:'Int_MetaAlloyHullReinforcement_Size2_Class1' }, // TODO: fdid
+		 9241 : { mtype:'imahrp', cost: 54000, name:'Meta Alloy Hull Reinforcement Package', class:2, rating:'D', mass:  2, hullrnf:171, caures:3.0, fdid:128793120, fdname:'Int_MetaAlloyHullReinforcement_Size2_Class2' }, // TODO: fdid
+		 9351 : { mtype:'imahrp', cost: 42000, name:'Meta Alloy Hull Reinforcement Package', class:3, rating:'E', mass:  8, hullrnf:207, caures:3.0, fdid:128793121, fdname:'Int_MetaAlloyHullReinforcement_Size3_Class1' }, // TODO: fdid
+		 9341 : { mtype:'imahrp', cost:126000, name:'Meta Alloy Hull Reinforcement Package', class:3, rating:'D', mass:  4, hullrnf:234, caures:3.0, fdid:128793122, fdname:'Int_MetaAlloyHullReinforcement_Size3_Class2' }, // TODO: fdid
+		 9451 : { mtype:'imahrp', cost: 97500, name:'Meta Alloy Hull Reinforcement Package', class:4, rating:'E', mass: 16, hullrnf:270, caures:3.0, fdid:128793123, fdname:'Int_MetaAlloyHullReinforcement_Size4_Class1' }, // TODO: fdid
+		 9441 : { mtype:'imahrp', cost:292500, name:'Meta Alloy Hull Reinforcement Package', class:4, rating:'D', mass:  8, hullrnf:297, caures:3.0, fdid:128793124, fdname:'Int_MetaAlloyHullReinforcement_Size4_Class2' }, // TODO: fdid
+		 9551 : { mtype:'imahrp', cost:225000, name:'Meta Alloy Hull Reinforcement Package', class:5, rating:'E', mass: 32, hullrnf:324, caures:3.0, fdid:128793125, fdname:'Int_MetaAlloyHullReinforcement_Size5_Class1' }, // TODO: fdid
+		 9541 : { mtype:'imahrp', cost:675000, name:'Meta Alloy Hull Reinforcement Package', class:5, rating:'D', mass: 16, hullrnf:351, caures:3.0, fdid:128793126, fdname:'Int_MetaAlloyHullReinforcement_Size5_Class2' }, // TODO: fdid
 
 		 9651 : { mtype:'ghrp', cost:  9750, name:'Guardian Hull Reinforcement Package', class:1, rating:'E', mass:  2, pwrdraw:0.45, boottime:0, hullrnf: 100, kinres:0.0, thmres:2.0, expres:0.0, caures:5.0, fdid:128833945, fdname:'Int_GuardianHullReinforcement_Size1_Class1' },
 		 9641 : { mtype:'ghrp', cost: 29250, name:'Guardian Hull Reinforcement Package', class:1, rating:'D', mass:  1, pwrdraw:0.56, boottime:0, hullrnf: 138, kinres:0.0, thmres:2.0, expres:0.0, caures:5.0, fdid:128833946, fdname:'Int_GuardianHullReinforcement_Size1_Class2' },
